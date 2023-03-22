66b98dccf1fb6d616d4f1a85f95cd7911a957851
Gunhoon Lee <gunhoon@naver.com>
2023-03-22 13:27:15 +0900

    Consuming a RESTful Web Service #3
    Finishing the Application

--- consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ consuming-rest/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -1,13 +1,38 @@
 package com.example.consumingrest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
+
 	public static void main(String[] args) {
+		log.info("1111");
 		SpringApplication.run(ConsumingRestApplication.class, args);
+		log.info("2222");
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		log.info("3333");
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		log.info("4444");
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://localhost:8080/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
 }
